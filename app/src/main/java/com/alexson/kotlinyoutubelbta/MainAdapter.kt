fb2ccafd5928719ff57bfd1b52167ccdcd8384a7
alexsonsilva <21643@student.dorset-college.ie>
2021-03-03 21:55:11 +0000

    Commit

---
+++ app/src/main/java/com/alexson/kotlinyoutubelbta/MainAdapter.kt
@@ -0,0 +1,61 @@
+package com.alexson.kotlinyoutubelbta
+
+import android.content.Intent
+import android.icu.number.NumberFormatter.with
+import android.icu.number.NumberRangeFormatter.with
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.video_row.view.*
+
+class MainAdapter(val homeFeed: HomeFeed): RecyclerView.Adapter<CustomViewHolder>(){
+
+    val videoTitles = listOf("First title", "Second", "3rd", "MOOOORE TITLE")
+
+    //numberOfItems
+    override fun getItemCount(): Int {
+        return homeFeed.videos.count()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+        val layoutInflater = LayoutInflater.from(parent?.context)
+        val cellForRow = layoutInflater.inflate(R.layout.video_row, parent, false)
+        return CustomViewHolder(cellForRow)
+    }
+
+    override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
+//        val videoTitle = videoTitles.get(position)
+        val video = homeFeed.videos.get(position)
+        holder?.view?.textView_video_title?.text = video.name
+
+        holder?.view?.textView_channel_name?.text = video.channel.name + " * " + "20K Views\n4 days ago"
+
+        val thumbnailImageView = holder?.view?.imageView_video_tumbnail
+        Picasso.with(holder?.view?.context).load(video.imageUrl).into(thumbnailImageView)
+
+        val channelProfileImageView = holder?.view?.imageView_channel_profile
+        Picasso.with(holder?.view?.context).load(video.channel.profileImageUrl).into(channelProfileImageView)
+
+        holder?.video = video
+    }
+}
+
+class CustomViewHolder(val view: View, var video: Video? = null): RecyclerView.ViewHolder(view){
+
+    companion object{
+        val VIDEO_TITLE_KEY = "VIDEO_TITLE"
+        val VIDEO_ID_KEY = "VIDEO_ID"
+    }
+        init {
+            view.setOnClickListener {
+                val intent = Intent(view.context, CourseDetailActivity::class.java)
+
+                intent.putExtra(VIDEO_TITLE_KEY, video?.name)
+                intent.putExtra(VIDEO_ID_KEY, video?.id)
+
+                view.context.startActivity(intent)
+            }
+        }
+}
